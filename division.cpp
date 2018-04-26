20141ea39f8c36d8cdc2f1413b96baea019d5324
Tasrin Sultana <tasrinsultanahappy@gmail.com>
2018-04-26 10:16:25 +0600

    Add division program

---
+++ division.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	float div;
+
+	cout << "Enter your number:"<< endl;
+	cin >> a >> b;
+	div = a/b ;
+	cout << "ans = "<< div << endl;
+
+return 0;
+}
